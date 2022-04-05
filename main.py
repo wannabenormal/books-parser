74f8c954b12ab33031be1351f0d955478765fb49
Alexey Pesotsky <pesthedog@mail.ru>
2022-04-05 15:33:55 +0400

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+from pathlib import Path
+
 import requests
 
 
 def main():
-    url = 'https://tululu.org/txt.php?id=32168'
-    response = requests.post(url)
-    response.raise_for_status()
+    Path("./books").mkdir(parents=True, exist_ok=True)
 
-    with open('mars.txt', 'wb') as book:
-        book.write(response.content)
+    for book_id in range(1, 11):
+        url = f'https://tululu.org/txt.php?id={book_id}'
+        response = requests.post(url)
+        response.raise_for_status()
+
+        with open(f'./books/id{book_id}.txt', 'wb') as book:
+            book.write(response.content)
 
 
 if __name__ == '__main__':
