01faa0e1002223ca54c16a3a6754efd3fc2a4a60
Alexey Pesotsky <pesthedog@mail.ru>
2022-04-05 18:48:06 +0400

    Download book covers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from urllib.parse import urljoin, urlsplit, unquote
 
 import requests
 from pathvalidate import sanitize_filename, sanitize_filepath
@@ -20,7 +21,7 @@ def download_txt(url, filename, folder='books/'):
     Returns:
         str: Путь до файла, куда сохранён текст.
     """
-    response = requests.post(url)
+    response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     Path(folder).mkdir(parents=True, exist_ok=True)
@@ -29,6 +30,32 @@ def download_txt(url, filename, folder='books/'):
     filename_sanitized = sanitize_filename(f'{filename}.txt')
 
     path_to_save = os.path.join(path_sanitized, filename_sanitized)
+
+    with open(path_to_save, 'wb') as txt:
+        txt.write(response.content)
+
+    return path_to_save
+
+
+def download_image(url, folder='images/'):
+    """Функция для скачивания изоборажений.
+    Args:
+        url (str): Cсылка на изображение, которое хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранено изображение.
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    image_path = urlsplit(unquote(url)).path
+    image_name = image_path.split('/')[-1]
+
+    path_sanitized = sanitize_filepath(folder)
+    image_name_sanitized = sanitize_filename(image_name)
+
+    path_to_save = os.path.join(path_sanitized, image_name_sanitized)
 
     with open(path_to_save, 'wb') as txt:
         txt.write(response.content)
@@ -46,10 +73,15 @@ def parse_book_page(url):
     book_title, book_author = title_tag.text.split('::')
     book_title = book_title.strip()
     book_author = book_author.strip()
+    book_image_relative_src = soup.find(
+        'div', class_='bookimage'
+    ).find('img')['src']
+    book_image_src = urljoin(url, book_image_relative_src)
 
     return {
         'book_title': book_title,
-        'book_author': book_author
+        'book_author': book_author,
+        'book_image': book_image_src
     }
 
 
@@ -62,6 +94,8 @@ def main():
             book_info = parse_book_page(book_page_url)
         except requests.HTTPError:
             continue
+
+        download_image(book_info['book_image'])
 
         try:
             download_txt(
