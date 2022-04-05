2f65ce4f56f2c4800c28d6cff3448c3b3c3aa112
Alexey Pesotsky <pesthedog@mail.ru>
2022-04-05 17:49:23 +0400

    Download books with titles

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 from pathlib import Path
 
 import requests
+from pathvalidate import sanitize_filename, sanitize_filepath
+from bs4 import BeautifulSoup
 
 
 def check_for_redirect(response):
@@ -8,21 +11,65 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def main():
-    Path("./books").mkdir(parents=True, exist_ok=True)
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    response = requests.post(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    Path(folder).mkdir(parents=True, exist_ok=True)
 
+    path_sanitized = sanitize_filepath(folder)
+    filename_sanitized = sanitize_filename(f'{filename}.txt')
+
+    path_to_save = os.path.join(path_sanitized, filename_sanitized)
+
+    with open(path_to_save, 'wb') as txt:
+        txt.write(response.content)
+
+    return path_to_save
+
+
+def parse_book_page(url):
+    response = requests.get(url, allow_redirects=False)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('h1')
+    book_title, book_author = title_tag.text.split('::')
+    book_title = book_title.strip()
+    book_author = book_author.strip()
+
+    return {
+        'book_title': book_title,
+        'book_author': book_author
+    }
+
+
+def main():
     for book_id in range(1, 11):
-        url = f'https://tululu.org/txt.php?id={book_id}'
-        response = requests.post(url, allow_redirects=False)
-        response.raise_for_status()
+        book_page_url = f'https://tululu.org/b{book_id}/'
+        book_txt_url = f'https://tululu.org/txt.php?id={book_id}'
 
         try:
-            check_for_redirect(response)
+            book_info = parse_book_page(book_page_url)
         except requests.HTTPError:
             continue
 
-        with open(f'./books/id{book_id}.txt', 'wb') as book:
-            book.write(response.content)
+        try:
+            download_txt(
+                book_txt_url,
+                '{}. {}'.format(book_id, book_info['book_title'])
+            )
+        except requests.HTTPError:
+            continue
 
 
 if __name__ == '__main__':
